e138338d45a116ee6cf7799d7a6dfa0cc488d88b
akshay <sarveakshay@gmail.com>
2022-08-23 00:59:16 +0530

    first commit

---
+++ JavaTutorials/src/javatutorials/DataTypeProgram.java
@@ -0,0 +1,43 @@
+package javatutorials;
+
+public class DataTypeProgram {
+
+	public static void main(String[] args) {
+		// byte datatype
+		byte month=11;
+		System.out.println("The month is "+ month);
+
+		// short datatype
+		short quantity=10000;
+		System.out.println("The quantity is "+ quantity);
+
+		// int datatype
+		int empNumber=166444;
+		System.out.println("The emp number is "+ empNumber);
+
+		// long data type - in the end include letter 'l' or 'L'
+		long profit=444444444444444444L;
+		System.out.println("The profile is "+ profit);
+		
+		// char data type - start with single quotes
+		char letter='A';
+		System.out.println("The letter is "+ letter);
+		
+		// float data type - in the end include letter 'f'
+		float pieValue=3.14f;
+		System.out.println("The pievalue is "+ pieValue);
+		
+		// boolean data type
+		boolean status=false;
+		System.out.println("The status is "+ status);
+		
+		// double data type
+		double pieValue1=3.1444444444444d;
+		System.out.println("The pieValue1 is "+ pieValue1);
+		
+		// string data type
+		String msg="Welcome to Java";
+		System.out.println("The msg is "+ msg);
+	}
+
+}
---
+++ JavaTutorials/src/javatutorials/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package javatutorials;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// Welcome to Java Class
+		System.out.println("Welcome to Java Class");
+
+	}
+
+}
